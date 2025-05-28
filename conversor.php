c8fb25f31c8c1f71caf6feca43362bbd5483d8d0
sofiavitoriaa <sofiavitoria4331@gmail.com>
2025-05-28 20:08:45 -0300

    css

---
+++ conversor.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="gatinhaa.png" type="image/x-icon">
+    <title> Conversor de Moedas </title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <main>
+        <h1> Conversor de Moedas </h1>
+        <?php 
+            $inicio = date("m-d-Y", strtotime("-7 days"));
+            $fim = date("m-d-Y");
+            $url = 'https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarPeriodo(dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?@dataInicial=\''. $inicio .'\'&@dataFinalCotacao=\''. $fim . '\'&$top=1&$orderby=dataHoraCotacao%20desc&$format=json&$select=cotacaoCompra,dataHoraCotacao';
+
+            $dados = json_decode(file_get_contents($url), true);
+            //var_dump($dados);
+
+            $cotação = $dados["value"][0]["cotacaoCompra"];
+            $real = $_REQUEST["din"] ?? 0;
+            $dólar = $real / $cotação;
+            $padrão = numfmt_create("pt_BR", NumberFormatter::CURRENCY);
+            echo "<p>  Seus " . numfmt_format_currency($padrão, $real, "BRL") . " equivalem a <strong>" . numfmt_format_currency($padrão, $dólar, "USD") . "</strong> </p>"; 
+        ?>
+        <button onclick="javascript:history.go(-1)"> Voltar </button>
+    </main>
+</body>
+</html>
